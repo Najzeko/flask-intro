b60c355df7063e97f4a914edea8e199384c482f5
Marko Miletic <markom@novuscom.net>
2023-02-28 02:02:15 -0800

    tool to import data from xlsx

---
+++ import_data2.py
@@ -0,0 +1,9 @@
+import sqlite3
+import pandas as pd
+
+connection = sqlite3.connect("LumberFut.db")
+cursor = connection.cursor()
+
+data = pd.read_excel("LumberFut.xlsx", engine='openpyxl')
+data.to_sql("FuturesTable", connection, if_exists='replace')
+connection.close()
