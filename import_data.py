1536aacd7bb63bfb68f84aae52dd5e1fee18a305
Marko Miletic <markom@novuscom.net>
2023-03-01 04:33:52 -0800

    adjust for file move

--- import_data.py
+++ import_data.py
@@ -4,6 +4,6 @@ import pandas as pd
 connection = sqlite3.connect("LumberFut.db")
 cursor = connection.cursor()
 
-data = pd.read_excel("LumberFut.xlsx", engine='openpyxl')
+data = pd.read_excel("data/LumberFut.xlsx", engine='openpyxl')
 data.to_sql("FuturesTable", connection, if_exists='replace')
 connection.close()
